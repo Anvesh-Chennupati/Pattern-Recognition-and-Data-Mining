2c329d8ca6e8fb289cac7d0ba96d1f850b7cc692
Anvesh <anveshchowdary01@gmail.com>
2019-01-16 12:22:52 -0800

    some updates

--- Isolation_Forest/mainv2.cpp
+++ Isolation_Forest/mainv2.cpp
@@ -3,8 +3,9 @@
 #include<string>
 #include<sstream>
 #include<iterator>
+#include<algorithm>
+#include<queue>
 #include "isolation_forest.h"
-
 using namespace std;
 
 vector<vector<double> > readData(){
@@ -65,14 +66,9 @@ void validateInput(vector<vector<double> > input){
         }
     }
 }
-int main(){
-    vector<vector<double> > input;
-    int k;
-    cin>>k;
-    // cout<<k<<endl;
-    input = readData();
-    // displayData(input);
-    validateInput(input);
+
+
+void buildForest(vector<vector<double> > input,int k){
     vector<array<double,2> >data(input.size(),{0,0});
 
     int j=0;
@@ -89,7 +85,7 @@ int main(){
 	if (!forest.Build(50, 12345, data, 100))
 	{
 		std::cerr << "Failed to build Isolation Forest.\n";
-		return 1;
+		exit(1);
 	}
 
 	std::vector<double> anomaly_scores;
@@ -97,14 +93,54 @@ int main(){
 	if (!forest.GetAnomalyScores(data, anomaly_scores))
 	{
 		std::cerr << "Failed to calculate anomaly scores.\n";
-		return 2;
+		exit(2);
 	}
 
-	for (int i = 0; i < anomaly_scores.size(); i++)
-	{
-		std::cout << "Anomaly_score[" << i << "] " << anomaly_scores[i] << "\n";
-	}
+	// for (int i = 0; i < anomaly_scores.size(); i++)
+	// {
+	// 	std::cout << "Anomaly_score[" << i << "] " << anomaly_scores[i] << "\n";
+	// }
 
+    //Adding anomaly score to the input vector
+    // for(int i =0; i<input.size();i++){
+    //     input[i].push_back(anomaly_scores[i]);
+    // }
+    // cout<<"Before sorting data"<<endl;
+
+    // displayData(input);
+
+    priority_queue<pair<double, int> > pq; 
+	for (auto i = 0; i < anomaly_scores.size(); i++)
+	 {
+	 	pq.push(make_pair(anomaly_scores[i], i)); 
+	    // cout << "Anomaly_score[" << i << "] " << anomaly_scores[i] << "\n";
+	 }
+	while(k--){
+		pair<double, int> top = pq.top(); 
+		// cout<<top.first<<" "<<top.second<<" ";
+
+		for(int i=0; i<input[top.second].size(); i++){
+			cout<<input[top.second][i]<<" ";
+		}
+        cout<<endl;
+		pq.pop();
+	}
+    // cout<<"After sorting data"<<endl;
+    // sort(input.begin(),input.end(),sortcol);
+    // sort(input.begin(),input.end());
+}
+
+int main(){
+    vector<vector<double> > input;
+    int k;
+    cin>>k;
+    // cout<<k<<endl;
+    input = readData();
+    cout.precision(20);
+    // displayData(input);
+    validateInput(input);
+    buildForest(input,k);
+    
 	return 0;
 
 }
